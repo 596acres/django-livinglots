412a5d23606a363569dcb21b6ebfe0f3348ca26e
Eric Brelsford <ebrelsford@gmail.com>
2013-09-29 12:39:17 -0400

    get_model methods

--- livinglots/__init__.py
+++ livinglots/__init__.py
@@ -1,1 +1,5 @@
 __version__ = '0.1'
+
+
+# Import fetchers for convenience
+from models import get_lot_model, get_owner_model
--- livinglots/models.py
+++ livinglots/models.py
@@ -1,1 +1,21 @@
-# This space intentionally left blank
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import get_model as django_get_model
+
+
+def get_model(name):
+    try:
+        m = settings.LIVING_LOTS['MODELS']['lot']
+        return django_get_model(*m.split('.'))
+    except Exception:
+        raise ImproperlyConfigured(('Could not find a %s model. Did you set '
+                                    'LIVING_LOTS.MODELS.%s in your '
+                                    'settings.py?') % (name, name))
+
+
+def get_lot_model(name):
+    return get_model('lot')
+
+
+def get_owner_model(name):
+    return get_model('owner')
